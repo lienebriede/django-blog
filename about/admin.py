e64f64a549d93865b9899e9575ae88df7c549582
Liene Briede <lienebriede84@gmail.com>
2024-05-07 12:20:16 +0000

    Add Collaborate request model

--- about/admin.py
+++ about/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import About
+from .models import About, CollaborateRequest
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
     summernote_fields = ('title', 'content',)
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+    list_display = ('message', 'read',)
