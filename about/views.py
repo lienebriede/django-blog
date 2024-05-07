9bda4dc4f7428a7acf3b128c9b387a17c7748e07
Liene Briede <lienebriede84@gmail.com>
2024-05-07 12:33:06 +0000

    Add post to collaboaration form

--- about/views.py
+++ about/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from .models import About
 from .forms import CollaborateForm
+from django.contrib import messages
 
 # Create your views here.
 def about_me(request):
 
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            collaborate_form = CollaborateForm()
+            messages.add_message(request, messages.SUCCESS, 'Collaboration request received! I endeavour to respond within 2 working days.')
 
     return render(
         request,
